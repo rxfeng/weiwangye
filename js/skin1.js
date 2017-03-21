a32725b2fdfc5c289373691ecd9703170e001385
rxfeng <473867601@qq.com>
2017-03-21 18:28:41 +0800

    2017-3-21

---
+++ js/skin1.js
@@ -0,0 +1,11 @@
+//	换肤
+	if(10>1){
+			var css=document.createElement("link");
+		    css.href = "../css/skin-1.css";
+		    css.rel = "stylesheet";
+		    css.type = "text/css";
+		    document.getElementsByTagName('head').item(0).appendChild(css); 
+		    //		    换底部图片
+        	$($('#footer>div')[0]).children().attr('src','../img/voice-1.png')   
+        	$($('#footer>div')[2]).children().attr('src','../img/send-1.png') 
+	}
